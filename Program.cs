57dd227f0189173702d0e591d3bd9fe663794d4d
Halvorson <34112@nelnet.net>
2021-06-24 15:50:13 -0500

    initial coding

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace VehicleLot
+{
+    class Program
+    {
+        public class Vehicle 
+        {
+            protected int wheels(int count)
+            {
+                return 4;
+            }
+
+            protected int doors(int count)
+            {
+                return 2;
+            }
+        }
+        public class Car : Vehicle
+        {
+            public void doorCount()
+            {
+                doors(4);
+            }
+        }
+
+        public class Semi : Vehicle
+        {
+            public void wheelCount()
+            {
+                wheels(18);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            // myCar.wheelCount();
+            myCar.doorCount();
+
+
+            Console.WriteLine(myCar);
+            // BaseClass myBase = new BaseClass();
+        }
+    }
+}
